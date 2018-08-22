e28690df548d051420191f3c1c55db1fcd932f4e
Kamil Brodziak <kamil98brodziak@gmail.com>
2018-08-22 14:32:29 +0200

    Added Polybius cipher.

--- Test.java
+++ Test.java
@@ -28,6 +28,20 @@ public class Test
                     }
                     sscc(args[0], args[2].toLowerCase(), message);
                     break;
+                case "VIGENERE":
+                    if (args.length < 3 || !isAlpha(args[2])) {
+                        System.out.println("You must give a key that contains only letters!");
+                        break;
+                    }
+                    vig(args[0], args[2].toLowerCase(), message);
+                    break;
+                case "POLYBIUS":
+                if (args.length < 3 || !isAlpha(args[2])) {
+                    System.out.println("You must give a key that contains only letters!");
+                    break;
+                }
+                poly(args[0], args[2].toLowerCase(), message);
+                    break;
                 default:
                     System.out.println("There is no such option!");
             }
@@ -58,5 +72,23 @@ public class Test
     private static void sscc(String option, String key, String message) {
         SimpleSubstitutionChiper ssc = new SimpleSubstitutionChiper(key, message, option);
         ssc.Encipher();
+    }
+
+    private static void vig(String option, String key, String message) {
+        if (option.equals("-d"))
+            Vigenere.decrypt(key, message);
+        else if (option.equals("-e"))
+            Vigenere.encrypt(key, message);
+        else
+            System.out.println("No such option, available: -d, -e!");
+    }
+
+    private static void poly(String option, String key, String message) {
+        if (option.equals("-d"))
+            Polybius.decrypt(key.toLowerCase(), message.toUpperCase());
+        else if (option.equals("-e"))
+            Polybius.encrypt(key.toLowerCase(), message.toLowerCase());
+        else
+            System.out.println("No such option, available: -d, -e!");
     }
 }
